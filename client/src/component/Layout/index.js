5c1253183b87f30390e75565b69f9feba5514639
gughan <aegughan@gmail.com>
2025-04-29 00:22:27 +0530

    redirection on token expired

--- client/src/component/Layout/index.js
+++ client/src/component/Layout/index.js
@@ -1,12 +1,19 @@
 "use client";
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../Header'
-import { usePathname } from 'next/navigation';
-
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function Layout(props) {
     const { children } = props
+    const router = useRouter();
     const pathname = usePathname();
+    useEffect(() => {
+        const token = localStorage.getItem("userToken")
+        if (!token) {
+            router.push("/login")
+        }
+    }, [])
+
     return (
         <>
             {!(pathname?.includes("/login") || pathname?.includes("/signup")) && <Header />}
